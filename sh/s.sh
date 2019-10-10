0ca47bad9067ec75d9031badf0e1ebf5234cb60c
Spencer Brown <spencer@spencerbrown.org>
2019-10-10 13:58:32 -0500

    update sharded setup for new location of data directory

--- sh/s.sh
+++ sh/s.sh
@@ -2,7 +2,7 @@
 
 CONFIG=${1-m}
 
-mkdir -p {data/shard1,data/shard2,data/config,data/router}
+mkdir -p {../data/shard1,../data/shard2,../data/config,../data/router}
 
 sed 's/shard1/shard2/g; s/27018/27028/g; s/rs1/rs2/g' sh/${CONFIG}shard1.yaml > sh/${CONFIG}shard2.yaml
 
