b96747f92bc5e72e95a42b4d29c11346c1fedf94
Spencer Brown <spencer@spencerbrown.org>
2021-03-01 16:28:23 -0600

    fix bug in zero-based numbering

--- sh/s.sh
+++ sh/s.sh
@@ -2,7 +2,7 @@
 
 CONFIG=${1-m}
 
-mkdir -p {../data/shard1,../data/shard2,../data/shard3,../data/config,../data/router}
+mkdir -p {../data/shard0,../data/shard1,../data/shard2,../data/config,../data/router}
 
 sed 's/shard0/shard1/g; s/27018/27028/g; s/rs0/rs1/g' sh/${CONFIG}shard0.yaml > sh/${CONFIG}shard1.yaml
 sed 's/shard0/shard2/g; s/27018/27038/g; s/rs0/rs2/g' sh/${CONFIG}shard0.yaml > sh/${CONFIG}shard2.yaml
