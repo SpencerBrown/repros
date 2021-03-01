74c2f9b9af5b660599df998e3c6fe2901e6dae95
Spencer Brown <spencer@spencerbrown.org>
2021-03-01 15:57:02 -0600

    Start updating sharded cluster init for more flexibility and less copied code

--- sh/init.js
+++ sh/init.js
@@ -1,22 +1,62 @@
 // initiate config and shard replica sets
 
+function initialize_replica_set (name, host, config) {
+    print("\nInitializing replica set " + name + " with member " + host);
+
+    let rdb = new Mongo(host).getDB('admin');
+
+    rdb.runCommand({replSetInitiate: config});
+
+    print("\nWaiting for replica set " + name + " to become healthy...");
+
+    while (true) {
+        sleep(500);
+        ismast = rdb.isMaster();
+        if (ismast.ismaster) {
+            break;
+        }
+    }
+
+    print("\nReplica set " + name + " is healthy, primary is " + ismast.primary);
+}
+
+// hostname = getHostName();
+hostname = 'mongodb-local.computer';
+
+s_port = 27017;
+s_host = hostname + ':' + s_port;
+
+c_replsetname = 'cs';
+c_port = 27019;
+c_host = hostname + ':' + c_port;
+c_config = {
+    _id: c_replsetname, members: [
+        {_id: 0, host: c_host}
+    ]
+};
+
+num_shards = 3;
+shard_replsetnames = ['rs0', 'rs1', 'rs2'];
+shard_ports = [27018, 27028, 27038];
+shard_hosts = [];
+shard_configs = [];
+for (i=0; i<num_shards; i++) {
+    shard_hosts[i] = hostname + shard_ports[i];
+    shard_configs[i] = {
+        _id: shard_replsetnames[i],
+        members: [{_id: 0, host: shard_hosts[i]}]
+    };
+}
 shard1_replsetname = 'rs1';
 shard2_replsetname = 'rs2';
 shard3_replsetname = 'rs3';
-config_replsetname = 'cs';
-// hostname = getHostName();
-hostname = 'mongodb-local.computer';
-s_port = 27017;
 d1_port = 27018;
 d2_port = 27028;
 d3_port = 27038;
-c_port = 27019;
 
-s_host = hostname + ':' + s_port;
 d1_host = hostname + ':' + d1_port;
 d2_host = hostname + ':' + d2_port;
 d3_host = hostname + ':' + d3_port;
-c_host = hostname + ':' + c_port;
 
 au = {
     user: 'admin',
@@ -41,29 +81,7 @@ d3_config = {
     ]
 };
 
-print("\nInitializing config replica set \"" + config_replsetname + '" with members ' + c_host);
-
-c_config = {
-    _id: config_replsetname, members: [
-        {_id: 0, host: c_host}
-    ]
-};
-
-c_db = new Mongo(c_host).getDB('admin');
-
-c_db.runCommand({replSetInitiate: c_config});
-
-print("\nWaiting for replica set to become healthy...");
-
-while (true) {
-    sleep(1000);
-    ismast = c_db.isMaster();
-    if (ismast.ismaster) {
-        break;
-    }
-}
-
-print("\nReplica set healthy, primary is " + ismast.primary);
+initialize_replica_set(c_replsetname, c_host, c_config);
 
 print("\nInitializing replica set \"" + shard1_replsetname + '" with members ' + d1_host);
 d1_db = new Mongo(d1_host).getDB('admin');
