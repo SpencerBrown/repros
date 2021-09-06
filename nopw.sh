db800b1180fdf666023410519ebca0870853c13b
Spencer Brown <spencer@spencerbrown.org>
2021-09-06 14:40:01 -0500

    Complete conversion to mongotls-generated certs and keys

--- nopw.sh
+++ nopw.sh
@@ -1,3 +1,3 @@
-mongostat --port 27017 --host mongodb-local.computer --ssl --sslCAFile tls-certs/public-ca.pem --sslPEMKeyFile tls-certs/client-key-cert.pem -u admin --authenticationDatabase admin 1 <<EOF
+mongostat --port 27017 --host mongodb-local.computer --ssl --sslCAFile tls/root-ca.pem --sslPEMKeyFile tls/client-key-cert.pem -u admin --authenticationDatabase admin 1 <<EOF
 $(cat passwordfile)
 EOF
--- st/s.sh
+++ st/s.sh
@@ -6,7 +6,5 @@ mkdir -p ../data/m
 mongod -f st/${CONFIG}.yaml
 
 mongosh --tls --tlsCAFile tls/root-ca.pem --tlsCertificateKeyFile=tls/client-key-cert.pem  --host mongodb-local.computer --port 27017 st/init.js
-#mongosh --tls --tlsCAFile tls-certs/public-ca.pem --tlsCertificateKeyFile tls-certs/client-key-cert.pem --host mongodb-local.computer --port 27017 st/init.js
 
 mongosh --tls --tlsCAFile tls/root-ca.pem --tlsCertificateKeyFile=tls/client-key-cert.pem --host mongodb-local.computer --port 27017 --username admin --password tester --authenticationDatabase admin
-#mongosh --tls --tlsCAFile tls-certs/public-ca.pem --tlsCertificateKeyFile tls-certs/client-key-cert.pem --host mongodb-local.computer --port 27017 --username admin --password tester --authenticationDatabase admin
